09d53b600556d4fa8d09bb71214611b4973602f1
Zac Zhang <zac.z.zhang@outlook.com>
2022-11-02 16:27:50 +0100

    simulation_v2

--- simulation_v2/send.py
+++ simulation_v2/send.py
@@ -4,31 +4,15 @@ import simple_ur_script as ur
 reload(ur)
 import simple_comm as c
 reload(c)
-import math as m
-from compas.geometry import Frame
-from compas_rhino.conversions import frame_to_rhino
-
-
-# change these values if needed
-# ROBOT_IP = "192.168.10.10"
-SAFE_ROBOT_ACC = 0.1
-SAFE_ROBOT_VEL = 0.1
-IO = 0 # 0
-sleep_time = 0.2
 
 def tcp(script):
-    # script += ur.set_tcp_by_angles(-0.35, 0.5, 89.08, m.radians(0.0), m.radians(3.1415),m.radians(0.0))
-    # script += ur.set_tcp_by_angles(-0.35, 0.5, 89.08, m.radians(0.0), m.radians(0),m.radians(0.0))
-    script += ur.set_tcp_by_angles(-0.35, 0.5, 89.08, 0.0, 3.1415, 1.57)
-    
+    script += ur.set_tcp_by_angles(TCP[0], TCP[1], TCP[2], TCP[3], TCP[4], TCP[5])
     return script
 
 def set_robot_base():
-    pt_0 = rg.Point3d(-200 ,-320 ,-587.3) # base plane origin
-    # pt_1 = rg.Point3d(-206.2 ,-192.6 ,-587.3) # point on positive x axis
-    # pt_2 = rg.Point3d(456.4 ,-326.4 ,-587.3) # point on positive xy plane
-    pt_1 = rg.Point3d(-201.8 ,-519.9 ,-587.3) 
-    pt_2 = rg.Point3d(200 ,-323.9 ,-587.3) 
+    pt_0 = TABLE_NAVIGATION_POINTS[0] # base plane origin
+    pt_1 = TABLE_NAVIGATION_POINTS[1] 
+    pt_2 = TABLE_NAVIGATION_POINTS[2]
     robot_base = rg.Plane(pt_0,pt_1-pt_0,pt_2-pt_0)
     return robot_base
 
@@ -45,7 +29,6 @@ def pickup_brick(script,pick_up_plane):
     SAFE_DIST = 150
     
     safe_plane = pick_up_plane.Clone()
-    #safe_plane.Translate(rhinoplane.ZAxis*SAFE_DIST)
     safe_plane.Translate(rg.Vector3d.ZAxis*SAFE_DIST)
     
     ## add to the path: go to the safe plane
@@ -58,7 +41,7 @@ def pickup_brick(script,pick_up_plane):
     
     ## add to the path: turn on the vacuum and wait 1 sec.
     script += ur.set_digital_out(IO,True)
-    script += ur.sleep(sleep_time)
+    script += ur.sleep(SAFE_SLEEP_TIME)
 
     # add to the path: go back to the safe_plane
     script += ur.move_l(safe_plane, SAFE_ROBOT_ACC, SAFE_ROBOT_VEL)
@@ -68,8 +51,7 @@ def pickup_brick(script,pick_up_plane):
 def place_brick(script, place_plane):
     
     planes = []
-    
-    SAFE_DIST = 100
+
     safe_plane = place_plane.Clone()
     safe_plane.Translate(rg.Vector3d.ZAxis*SAFE_DIST)
     
@@ -83,7 +65,7 @@ def place_brick(script, place_plane):
     
     ## add to the path: turn off the vacuum and wait 1 second (hold the brick)
     script += ur.set_digital_out(IO,False)
-    script += ur.sleep(sleep_time)
+    script += ur.sleep(SAFE_SLEEP_TIME)
     # add to the path: go back to the safe plane
     script += ur.move_l(safe_plane, SAFE_ROBOT_ACC, SAFE_ROBOT_VEL)
     planes.append(safe_plane)
@@ -98,31 +80,14 @@ def send(script):
 script = ""
 script = tcp(script)
 
-#! ---------------------------------------------------------------- Test and navigation only: go to a point 
-#test_plane = debug_plane.Clone()
-#script += ur.move_l(rhino_to_robot_space(test_plane), SAFE_ROBOT_ACC, SAFE_ROBOT_VEL)
-#! ----------------------------------------------------------------
+if is_debug_mode:
+    test_plane = debug_plane.Clone()
+    script += ur.move_l(rhino_to_robot_space(test_plane), SAFE_ROBOT_ACC, SAFE_ROBOT_VEL)
 
+else:
+    for i in range(len(brick_planes)):
+        script, p = pickup_brick(script,rhino_to_robot_space(picking_planes[i%len(picking_planes)]))
+        script, p = place_brick(script,rhino_to_robot_space(brick_planes[i]))
 
-#!  ---------------------------------------------------------------- Real run
-#Optional: location of a pick up station
-# pick_point = Frame.from_axis_angle_vector([2.403,2.421,-4.166],[677.46,-886.44,-492.82])
-
-for i in range(len(brick_planes)):
-    script, p = pickup_brick(script,rhino_to_robot_space(picking_planes[i%len(picking_planes))])
-    script, p = place_brick(script,rhino_to_robot_space(brick_planes[i]))
-#!  ---------------------------------------------------------------- 
-
-#! Never touch
 if fabricate:
     send(script)
-robot_base_plane = set_robot_base()
-
-
-# Questions to ask:
-# Visualize gcode in GH
-# Robot position in GH
-# TCP
-# import compas
-# pose = compas.geometry.Frame([pose[3],pose[4],pose[5]],[pose[0],pose[1],pose[2]])
-debug_plane = debug_plane
