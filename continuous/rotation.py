cb46ee69e7fd10a8182ef138732fb5f4c91198fc
Zac Zhang <zac.z.zhang@outlook.com>
2022-11-10 18:17:50 +0100

    Fix bugs

---
+++ continuous/rotation.py
@@ -0,0 +1,28 @@
+import copy
+import Rhino.Geometry as rg
+import math
+
+distance = []
+for p in x:
+    # distance.append(p.DistanceTo(y.Origin)
+    distance.append(round(p.ClosestPoint(y.Origin).DistanceTo(y.Origin)))
+
+
+layers = copy.deepcopy(distance)
+
+xx = list(set(layers))
+xx.sort()
+
+temp = -1
+for tx in xx:
+
+    for n, d in enumerate(distance):
+        if d == tx:
+            x[n].Transform(
+                rg.Transform.Rotation(temp * math.pi/90, x[n].ZAxis, x[n].Origin)
+            )  # angleRadians: float, rotationAxis: Vector3d, rotationCenter: Point3d
+    temp = -temp
+
+
+a = x
+# a = layers
