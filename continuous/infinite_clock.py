dcbec08655261ccfa261927a47defa900f5e6219
Zac Zhang <zac.z.zhang@outlook.com>
2022-11-11 11:43:47 +0100

    optimized final version

--- continuous/infinite_clock.py
+++ continuous/infinite_clock.py
@@ -1,49 +1,50 @@
+import math
 
 
-def getZ (Plane):
+def getZ(Plane):
     return Plane.Origin.Z
+
 
 Count = 10
 Tolerance = 0.01
 
+RemovePlanes = []
+TargetPlanes = []
+for count in range(Count):
 
-# # dataDict = {}
-# if read:
-#     with open(path, "r") as f:
-#         dataDict = json.load(f)
+    # for i in range(
+    listLength = 100000000
+    for i in range(10):
+        length = len(list(data.Branch(i)))
+        if length < listLength:
+            listLength = length
 
-initPlanes =  list(data.Branch(count%10))
-targetPlanes = list(data.Branch((count+1)%10))
-    
-# for i in dataDict[str(count%Count)]:
-#     initPlanes.append(cv.plane_to_rhino(cg.Plane.from_jsonstring(i)))
-# for i in dataDict[str((count+1)%Count)]:
-#     targetPlanes.append(cv.plane_to_rhino(cg.Plane.from_jsonstring(i))) 
+    initPlanes = list(data.Branch((count) % Count))[:listLength]
+    theTargetPlanes = list(data.Branch((count + 1) % Count))[:listLength]
 
-iterations = 0
+    # for i in dataDict[str(count%Count)]:
+    #     initPlanes.append(cv.plane_to_rhino(cg.Plane.from_jsonstring(i)))
+    # for i in dataDict[str((count+1)%Count)]:
+    #     theTargetPlanes.append(cv.plane_to_rhino(cg.Plane.from_jsonstring(i)))
 
-keepPlanes = []
-removePlanes = []
-for n, p in enumerate(initPlanes):
-    temp = True
-    for m, i in enumerate(targetPlanes):
-        iterations +=1
-        if p.Origin.DistanceTo(i.Origin) < Tolerance:
-            # initPlanes.pop(n)
-            targetPlanes.pop(m) 
-            temp =False
-            keepPlanes.append(p)
-            break
-    if temp == True: removePlanes.append(p)
+    iterations = 0
 
-removePlanes.sort(key=getZ, reverse= True)
-targetPlanes.sort(key=getZ, reverse= False)
+    keepPlanes = []
+    theRemovePlanes = []
+    for n, p in enumerate(initPlanes):
+        temp = True
+        for m, i in enumerate(theTargetPlanes):
+            iterations += 1
+            if p.Origin.DistanceTo(i.Origin) < Tolerance:
+                # initPlanes.pop(n)
+                theTargetPlanes.pop(m)
+                temp = False
+                keepPlanes.append(p)
+                break
+        if temp == True:
+            theRemovePlanes.append(p)
 
-
-print (iterations)
-
-outTemp = keepPlanes
-
-
-initVisual = initPlanes
-targetVisual = targetPlanes
+    theRemovePlanes.sort(key=getZ, reverse=True)
+    theTargetPlanes.sort(key=getZ, reverse=False)
+    RemovePlanes.extend(theRemovePlanes)
+    TargetPlanes.extend(theTargetPlanes)
